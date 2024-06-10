aa945a1b53ed8d628fd15499420f68d0fb69b7f1
Miguel Ruiz <later481@gmail.com>
2024-06-09 22:33:01 -0600

    ejercicios 1-5

---
+++ day_1.js
@@ -0,0 +1,28 @@
+//Ejercicio 1: Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+//Ejercicio 2: Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+//Ejercicio 3: Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* Coments can make code readable, 
+easy to reuse and informative */
+
+//Ejercicio 3: Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let string_ej = "Hola";
+let boolean_ej = true;
+let undefined_ej = undefined;
+let null_ej = null;
+
+//Ejercicio 4: Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof(string_ej));
+console.log(typeof(boolean_ej));
+console.log(typeof(undefined_ej));
+console.log(typeof(null_ej));
+
+//Ejercicio 5: Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
