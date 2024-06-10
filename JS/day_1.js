01f0004850865c4d1167290fd64fd4f9feecdf6f
Miguel Ruiz <later481@gmail.com>
2024-06-09 22:50:03 -0600

    ejercicios 6-10

---
+++ JS/day_1.js
@@ -0,0 +1,50 @@
+//Ejercicio 1: Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+//Ejercicio 2: Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+//Ejercicio 3: Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* Coments can make code readable, 
+easy to reuse and informative */
+
+//Ejercicio 4: Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let string_ej = "Hola";
+let boolean_ej = true;
+let undefined_ej = undefined;
+let null_ej = null;
+
+//Ejercicio 5: Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof(string_ej));
+console.log(typeof(boolean_ej));
+console.log(typeof(undefined_ej));
+console.log(typeof(null_ej));
+
+//Ejercicio 6: Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
+//Ejercicio 7: Declare four variables with assigned values
+let var5 = 34;
+let var6 = 34;
+let var7 = 34;
+let var8 = 34;
+
+//Ejercicio 8: Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let first_name = "Miguel",
+    last_name = "Ruiz",
+    marital_status = "single",
+    country = "México",
+    age = 29
+
+//Ejercicio 9: Declare variables to store your first name, last name, marital status, country and age in a single line
+let first_name1 = "Miguel", last_name1 = "Ruiz", marital_status1 = "single", country1 = "México", age1 = 29
+
+//Ejercicio 10: Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 29,
+    yourAge = 30;
+
+console.log(myAge, yourAge);
