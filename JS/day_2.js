e81fa63c8c2faae6d1415c4d68845fdb7731ab35
Miguel Ruiz <later481@gmail.com>
2024-06-10 22:19:14 -0600

    ejercicios day 2 [1-5]

---
+++ JS/day_2.js
@@ -0,0 +1,14 @@
+//Ejercicio 1: Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = '30 Days Of JavaScript';
+
+//Ejercicio 2: Print the string on the browser console using console.log()
+console.log(challenge);
+
+//Ejercicio 3: Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
+
+//Ejercicio 4: Change all the string characters to capital letters using toUpperCase() method
+let challengeUpper = challenge.toUpperCase();
+
+//Ejercicio 5: Change all the string characters to lowercase letters using toLowerCase() method
+let challengeLower = challenge.toLowerCase();
