b750e89cbb5ba196bc7c433369c5a40d675bf036
Miguel Ruiz <later481@gmail.com>
2024-06-10 22:26:01 -0600

    ejercicios day 2 [6-10]

--- JS/day_2.js
+++ JS/day_2.js
@@ -9,6 +9,25 @@ console.log(challenge.length);
 
 //Ejercicio 4: Change all the string characters to capital letters using toUpperCase() method
 let challengeUpper = challenge.toUpperCase();
+console.log(challengeUpper)
 
 //Ejercicio 5: Change all the string characters to lowercase letters using toLowerCase() method
 let challengeLower = challenge.toLowerCase();
+console.log(challengeLower)
+
+//Ejercicio 6: Cut (slice) out the first word of the string using substr() or substring() method
+let challengeSubstr = challenge.substring(0, 3);
+console.log(challengeSubstr);
+
+//Ejercicio 7: Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+let challengeSlice = challenge.slice(3);
+console.log(challengeSlice);
+
+//Ejercicio 8: Check if the string contains a word Script using includes() method
+console.log(challenge.includes("Script"));
+
+//Ejercicio 9: Split the string into an array using split() method
+console.log(challenge.split());
+
+//Ejercicio 10: Split the string 30 Days Of JavaScript at the space using split() method
+console.log(challenge.split(" "));
