10f5a5eca5a788665658636c64fa80b3f0b6a153
Miguel Ruiz <later481@gmail.com>
2024-06-10 22:58:32 -0600

    ejercicios day 2 [16-25]

--- JS/day_2.js
+++ JS/day_2.js
@@ -49,3 +49,35 @@ console.log(challenge.charCodeAt("J"));
 console.log(challenge.indexOf("a"));
 
 //Ejercicio 16: Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+console.log(challenge.lastIndexOf("a"));
+
+//Ejercicio 17: Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let stringEj = 'You cannot end a sentence with because because because is a conjunction';
+console.log(stringEj.indexOf("because"));
+
+//Ejercicio 18: Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(stringEj.lastIndexOf("because"));
+
+//Ejercicio 19: Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction' 
+console.log(stringEj.search('because'));
+
+//Ejercicio 20: Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+let stringEj2 = ' 30 Days Of JavaScript ';
+console.log(stringEj2.trim());
+
+//Ejercicio 21: Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.startsWith("30"));
+
+//Ejercicio 22: Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.endsWith("ipt"));
+
+//Ejercicio 23: Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(challenge.match(/a/gi));
+
+//Ejercicio 24: Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+let days = '30 Days of',
+    js = ' JavaScript';
+console.log(days.concat(js));
+
+//Ejercicio 25: Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(challenge.repeat(2));
