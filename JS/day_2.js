1d3df0406246af7dd655ea62678a6c5dc7b2d0ba
Miguel Ruiz <later481@gmail.com>
2024-06-11 23:07:20 -0600

    ejercicios day 2 level 2 [1-5]

--- JS/day_2.js
+++ JS/day_2.js
@@ -81,3 +81,26 @@ console.log(days.concat(js));
 
 //Ejercicio 25: Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log(challenge.repeat(2));
+
+
+//--------------- Level 2 --------------------
+//Ejercicio 1: Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
+
+//Ejercicio 2: Using console.log() print out the following quote by Mother Teresa: "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+
+//Ejercicio 3: Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+console.log(typeof("10") == typeof(10));
+console.log(typeof(10) == typeof(10));
+
+//Ejercicio 4: Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+console.log(parseFloat('9.8') == 10);
+console.log(parseFloat('10') == 10);
+
+//Ejercicio 5: Check if 'on' is found in both python and jargon
+let pattern = /on/gi;
+let stringD = 'Python';
+let stringE = 'jargon';
+console.log(stringD.search(pattern));
+console.log(stringE.search(pattern));
