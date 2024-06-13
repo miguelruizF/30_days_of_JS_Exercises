f3f7906a93b7be17ae20c490a14845469ff533c9
Miguel Ruiz <later481@gmail.com>
2024-06-12 23:32:13 -0600

    ejercicios day 2 level 3 [1-4]

--- JS/day_2.js
+++ JS/day_2.js
@@ -129,10 +129,10 @@ console.log(stringJS.charAt(numRandom4));
 
 //Ejercicio 11: Use console.log() and escape characters to print the following pattern. 
 /*  1 1 1 1 1
-    2 1 2 4 8
-    3 1 3 9 27
-    4 1 4 16 64
-    5 1 5 25 125 */
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125 */
 
 console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
 
@@ -142,3 +142,34 @@ let patternS = stringB.indexOf('because');
 let patternT = stringB.lastIndexOf('e');
 let newString = stringB.substring(patternS,patternT + 1);
 console.log(newString);
+
+
+//--------------- Level 3 --------------------
+//Ejercicio 1: 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+let string1 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+console.log(string1.length);
+
+//Ejercicio 2: Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let string2 = 'You cannot end a sentence with because because because is a conjunction';
+let pattern1 = /because/gi;
+console.log(string2.match(pattern1));
+
+//Ejercicio 3: Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+let patternReplace = /\W/gi;
+let newString1 = sentence.replace(patternReplace, ' ').trim().replace(/^\s+|\s+$|\s+(?=\s)/g, "");
+let newString2 = newString1.split();
+const hasDuplicates = array => new Set(array).size < array.length
+console.log(hasDuplicates(newString2));
+
+//Ejercicio 4: Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let cadena = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+// let newArray = cadena.split(" ");
+let patron = /\d+/g;
+let ingresos = cadena.match(patron).map(Number);
+
+const suma1 = ingresos.reduce((valorAnterior, valorActual) => {
+    return valorAnterior + valorActual;
+}, 0);
+
+console.log(suma1);
