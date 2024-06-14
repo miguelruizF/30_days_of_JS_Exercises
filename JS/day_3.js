f80c3d073e1ee0185125c8c7f1dfd207dc6d2f78
Miguel Ruiz <later481@gmail.com>
2024-06-14 17:52:35 -0600

    Day 3 Ejercicio [1]

---
+++ JS/day_3.js
@@ -0,0 +1,17 @@
+//Ejercicio 1: Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = 'Miguel', 
+    lastName = "Ruiz", 
+    country = "México", 
+    city = "Puebla", 
+    age = 29, 
+    isMarried = false, 
+    year = 2024;
+
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(country));
+console.log(typeof(city));
+console.log(typeof(age));
+console.log(typeof(isMarried));
+console.log(typeof(isMarried));
+console.log(typeof(year));
