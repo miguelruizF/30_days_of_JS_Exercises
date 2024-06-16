384d9b7097999e58689807542c2ca2d46c05dc43
Miguel Ruiz <later481@gmail.com>
2024-06-15 22:12:39 -0600

    ejercicios day 3 Level 2 [1-4]

--- JS/day_3.js
+++ JS/day_3.js
@@ -58,14 +58,62 @@ console.log(4 == '4');
 console.log(4 === '4');
 
 //Ejercicio 6: Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
-/* 4 > 3 && 10 < 12
-4 > 3 && 10 > 12
-4 > 3 || 10 < 12
-4 > 3 || 10 > 12
-!(4 > 3)
-!(4 < 3)
-!(false)
-!(4 > 3 && 10 < 12)
-!(4 > 3 && 10 > 12)
-!(4 === '4')
-There is no 'on' in both dragon and python */
+/* 4 > 3 && 10 < 12 true
+4 > 3 && 10 > 12 false
+4 > 3 || 10 < 12 true
+4 > 3 || 10 > 12 true
+!(4 > 3) false
+!(4 < 3) true
+!(false) true
+!(4 > 3 && 10 < 12) false
+!(4 > 3 && 10 > 12) true
+!(4 === '4') true
+There is no 'on' in both dragon and python  false*/
+console.log(4 > 3 && 10 < 12);
+console.log(4 > 3 && 10 > 12);
+console.log(4 > 3 || 10 < 12);
+console.log(4 > 3 || 10 > 12);
+console.log(!(4 > 3));
+console.log(!(4 < 3));
+console.log(!(false));
+console.log(!(4 > 3 && 10 < 12));
+console.log(!(4 > 3 && 10 > 12));
+console.log(!(4 === '4'));
+console.log('dragon'.includes('on'));
+console.log('python'.includes('on'));
+
+//Ejercicio 7: Use the Date object to do the following activities
+/* What is the year today?
+What is the month today as a number?
+What is the date today?
+What is the day today as a number?
+What is the hours now?
+What is the minutes now?
+Find out the numbers of seconds elapsed from January 1, 1970 to now. */
+let date_info = new Date();
+console.log(date_info.getFullYear());
+console.log(date_info.getMonth());
+console.log(Date());
+console.log(date_info.getDay());
+console.log(date_info.getHours());
+console.log(date_info.getMinutes());
+console.log(date_info.getTime());
+
+//--------------- Level 2 --------------------
+//Ejercicio 1: Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h)
+/* let base = prompt('Enter base: ');
+let altura = prompt('Enter altura: ');
+alert(`The area of the triangle is ${(base*altura)/2}`) */
+
+//Ejercicio 2: Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+/* let lado_a = Number(prompt('Enter side a: '));
+let lado_b = Number(prompt('Enter side b: '));
+let lado_c = Number(prompt('Enter side c: '));
+alert(`The perimeter of the triangle is ${lado_a+lado_b+lado_c}`); */
+
+//Ejercicio 3: Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+/* let largo = Number(prompt('Enter length: '));
+let ancho = Number(prompt('Enter width: '));
+alert(`The area of the rectangle is ${largo*ancho}`); 
+alert(`The perimeter of the rectangle is ${2*(largo+ancho)}`);  */
+
