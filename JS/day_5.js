e6a506a9e3f5ad108e2754f50907621cde2a7808
Miguel Ruiz <later481@gmail.com>
2024-06-27 15:34:50 -0600

    Ejercicio dia 5 level 3 [1]

--- JS/day_5.js
+++ JS/day_5.js
@@ -149,3 +149,38 @@ if(prog_Tech.webTechs.includes("Sass")){
     prog_Tech.webTechs.push("Sass");
 }
 console.log(prog_Tech.webTechs);
+
+//Ejercicio 6: Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+let fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack)
+
+
+
+//--------------- Level 3 --------------------
+//Ejercicio 1: The following is an array of 10 students ages:
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+/* Sort the array and find the min and max age
+    Find the median age(one middle item or two middle items divided by two)
+    Find the average age(all items divided by number of items)
+    Find the range of the ages(max minus min)
+    Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array 
+*/
+const ordArr = [...ages].sort();
+console.log(ordArr);
+console.log(Math.min(...ordArr));
+console.log(Math.max(...ordArr));
+
+let values = ages.sort((a,b) => a-b);
+let bajo = Math.floor((values.length - 1) / 2);
+let alto = Math.ceil((values.length - 1) / 2);
+let median = (values[bajo] + values[alto] / 2);
+console.log(median);
+
+let countValues = ordArr.length;
+let sum = ordArr.reduce((prev, curr) => curr += prev);
+let media = sum / countValues;
+console.log(media)
