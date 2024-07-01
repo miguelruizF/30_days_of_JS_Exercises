113225f81c839b27c536d78160df436ae1927d01
Miguel Ruiz <later481@gmail.com>
2024-07-01 17:18:00 -0600

    Ejercicio dia 6 level 1 [1-2]

---
+++ JS/day_6.js
@@ -0,0 +1,36 @@
+//----------- LOOPS ---------------
+//Ejercicio 1: Iterate 0 to 10 using for loop, do the same using while and do while loop
+/* for (let i = 0; i <= 10; i++) {
+    console.log(i);
+} */
+
+/* let i= 0; 
+while (i <= 10) {
+    console.log(i);
+    i++;
+} */
+
+/* let i = 0;
+do {
+    console.log(i);
+    i++
+} while (i <= 10)
+ */
+
+//Ejercicio 2: Iterate 10 to 0 using for loop, do the same using while and do while loop
+let i = 10;
+/* for (let i = 10; i >= 0; i--) {
+    console.log(i);
+} */
+
+/* while (i >= 0) {
+    console.log(i);
+    i--;
+} */
+
+/* do {
+    console.log(i);
+    i--;
+} while (i >= 0); */
+
+//Ejercicio 3: Iterate 0 to n using for loop
