0d715fd14c55da7fe7e0353d3d28636279441dbb
Miguel Ruiz <later481@gmail.com>
2024-07-22 17:47:51 -0600

    Ejercicio dia 7 level 1 [6-10]

--- JS/day_7.js
+++ JS/day_7.js
@@ -30,3 +30,35 @@ function perimeterOfRectangle(length, width) {
     return 2*(length+width);
 }
 console.log(perimeterOfRectangle(10, 10));
+
+//Ejercicio 6: A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+function volumeOfRectPrism(length, width, height) {
+    return length * width * height;
+}
+console.log(volumeOfRectPrism(5,5,5));
+
+//Ejercicio 7: Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
+function areaOfCircle(radio) {
+    let PI = Math.PI;
+    return PI * ( radio * radio );
+}
+console.log(areaOfCircle(5));
+
+//Ejercicio 8: Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+function circumOfCircle(radio) {
+    let PI = Math.PI;
+    return (2 * PI) * radio;
+}
+console.log(circumOfCircle(5));
+
+//Ejercicio 9: Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+function density(mass, volume) {
+    return mass / volume;
+}
+console.log(density(555.265, 45456.2));
+
+//Ejercicio 10: Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+function speed(distance, time) {
+    return `${distance/time} m/s`;
+}
+console.log(speed(5,10));
