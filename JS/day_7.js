214ba64f91401c378b066e1206597f435c025aec
Miguel Ruiz <later481@gmail.com>
2024-07-22 16:17:30 -0600

    Ejercicio dia 7 level 1 [1-5]

--- JS/day_7.js
+++ JS/day_7.js
@@ -1,4 +1,32 @@
 //----------- FUNCTIONS ---------------
 //------------ lEVEL 1 ----------------
 
-//Ejercicio 1:
+//Ejercicio 1: Declare a function fullName and it print out your full name.
+function printFullName(name) {
+    console.log(name);
+}
+printFullName('Miguel Angel Ruiz');
+
+//Ejercicio 2: Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+function fullName(firstName, lastName) {
+    return(`${firstName} ${lastName}`);
+}
+console.log(fullName('Miguel Angel', 'Ruiz'));
+
+//Ejercicio 3: Declare a function addNumbers and it takes two two parameters and it returns sum.
+function addNumbers(one, two) {
+    return one + two;
+}
+console.log(addNumbers(5,20));
+
+//Ejercicio 4: An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+function areaOfRectangle(length, width) {
+    return length * width;
+}
+console.log(areaOfRectangle(15, 20));
+
+//Ejercicio 5: A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+function perimeterOfRectangle(length, width) {
+    return 2*(length+width);
+}
+console.log(perimeterOfRectangle(10, 10));
