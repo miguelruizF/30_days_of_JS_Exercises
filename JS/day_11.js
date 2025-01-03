18e42566df794f1f3f21d792ada17a0be62faea9
Miguel Ruiz <later481@gmail.com>
2025-01-03 16:53:59 -0600

    add day_11.js file

---
+++ JS/day_11.js
@@ -0,0 +1,4 @@
+//----------- Destructuring and Spreating ---------------
+//------------ LEVEL 1 ----------------
+
+//Ejercicio 1.1: Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
