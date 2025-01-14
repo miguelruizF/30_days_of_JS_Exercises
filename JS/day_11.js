08f2e0d402d9db4e4f84f0733a9786f84afecb6e
Miguel Ruiz <later481@gmail.com>
2025-01-13 22:49:26 -0600

    exercises day 11 level 3 [1-3] completed

--- JS/day_11.js
+++ JS/day_11.js
@@ -1,7 +1,5 @@
-import { constants, users } from "./Arrays/dataDay10.js";
-import { countries } from "./Arrays/dataDay10.js";
-import { rectangle } from "./Arrays/dataDay10.js";
-
+import { constants, users, countries, rectangle } from "./Arrays/dataDay10.js";
+import { countries_of_world } from "./Arrays/data/countries_data_day11.js";
 
 //----------- Destructuring and Spreating ---------------
 //------------ LEVEL 1 ----------------
@@ -34,3 +32,38 @@ for(const [key, value] of Object.entries(users)){
 
 
 //------------ LEVEL 3 ----------------
+//Ejercicio 1: Destructure the countries object print name, capital, population and languages of all countries
+for (const {name, capital, population, languages} of countries_of_world){
+    console.log(`name: ${name} - capital: ${capital} - population: ${population} - languages: ${languages}`);
+}
+
+//Ejercicio 2: A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+const [name, skills, score, jsScore = score[2], reactScore = score[3]] = student;
+console.log(name, skills, jsScore, reactScore);
+
+//Ejercicio 3: Write a function called convertArrayToObject which can convert the array to a structure object.
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+function convertArrayToObject(arr){
+    const arrOfStudents = []
+    arr.forEach(student => {
+        const [name, skills, scores] = student;
+        const objStudent = {
+            name: name,
+            skills: skills,
+            scores: scores
+        }
+        arrOfStudents.push(objStudent);
+    });
+    console.log(arrOfStudents);
+}
+convertArrayToObject(students);
+
+//Ejercicio 4: Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+/* Add Bootstrap with level 8 to the front end skill sets
+Add Express with level 9 to the back end skill sets
+Add SQL with level 8 to the data base skill sets
+Add SQL without level to the data science skill sets */
