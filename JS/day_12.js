ae5b069ba5760f93d2370a7f984d02671ae23261
Miguel Ruiz <later481@gmail.com>
2025-01-27 22:46:40 -0600

    exercises day 12 level 2 completed

--- JS/day_12.js
+++ JS/day_12.js
@@ -23,3 +23,27 @@ console.log(`The distance between the two furthest particles is ${distance}`);
 const regExp3 = /^[a-zA-Z_$][a-zA-Z_$0-9]*$/;
 const variable = 'first_name';
 console.log(regExp3.test(variable));
+
+
+//------------ LEVEL 2 ----------------
+
+//Ejercicio 1: Write a function called tenMostFrequentWords which get the ten most frequent word from a string?
+const paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
+const tenMostFrequentWords = (parameter) => {
+    const expRegForWords = /\w+/gi; //definir expresion regular
+    const resultOfExtract = parameter.match(expRegForWords); //extraer palabras
+    const ocurrencesArray = []; //Definir un array vacio donde se guardaran las palabras y su cantidad de ocurrencias
+    resultOfExtract.forEach(word => { //recorrer el array de palabras
+        let wordCount = 0; //inicializar contador de ocurrencias
+        ocurrencesArray.push({ word, wordCount }); //agregar palabra y contador al array
+        resultOfExtract.forEach(word2 => { //recorrer nuevamente el array de palabras
+            if (word === word2) { //si la palabra es igual a la palabra recorrida
+                wordCount++; //incrementar contador
+                ocurrencesArray.find(item => item.word === word).wordCount = wordCount; //actualizar contador en el array
+            }
+        });
+    });
+    ocurrencesArray.sort((a, b) => b.wordCount - a.wordCount); //ordenar array de mayor a menor
+    console.log(ocurrencesArray.slice(0, 10)); //mostrar las 10 primeras palabras
+}
+tenMostFrequentWords(paragraph);
