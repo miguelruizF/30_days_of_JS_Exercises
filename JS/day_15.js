e72d3ff3071d374e3b344f168db571341f539e1c
Miguel Ruiz <later481@gmail.com>
2025-02-07 17:32:31 -0600

    Day 15 Level 3 [1]

--- JS/day_15.js
+++ JS/day_15.js
@@ -51,3 +51,35 @@ const chihuahua = new Perro('Otso', 5, 'cafe', 4, 'pequeña');
 console.log(chihuahua);
 console.log(chihuahua.informacion());
 chihuahua.moreInfo();
+
+
+//------------ LEVEL 3 ----------------
+
+//Ejercicio 1: Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
+const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+class Statistics {
+    constructor(arr){
+        this.arr = arr
+    }
+    
+    get describe(){
+        return {
+            Count: this.count(),
+            Sum: this.sum()
+        }
+    }
+
+    count(){
+        const count = this.arr
+        return count.length
+    }
+    sum(){
+        let sum = 0;
+        this.arr.forEach(num => {
+            sum += num;
+        });
+        return sum;
+    }
+}
+const descripcion = new Statistics(ages);
+console.log(descripcion.describe)
