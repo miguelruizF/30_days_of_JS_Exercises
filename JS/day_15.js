2270ea901bb97756533710aabf89f4b70855fda8
Miguel Ruiz <later481@gmail.com>
2025-02-21 17:03:51 -0600

    change in JS/day_15.js

--- JS/day_15.js
+++ JS/day_15.js
@@ -142,4 +142,6 @@ class Statistics {
     }
 }
 const descripcion = new Statistics(ages);
-console.log(descripcion.describe())
+console.log(descripcion.describe());
+
+//Ejercicio 2: Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
