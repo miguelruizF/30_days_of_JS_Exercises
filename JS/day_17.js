95692776618fdf5e1d44186ee91cd97ff0d35cc3
Miguel Ruiz <later481@gmail.com>
2025-02-28 16:13:37 -0600

    Day 17 Level 3 completed

--- JS/day_17.js
+++ JS/day_17.js
@@ -25,3 +25,74 @@ const student = {
 }
 const studentStringify = JSON.stringify(student, undefined, 4);
 localStorage.setItem("student", studentStringify);
+
+
+//------------ LEVEL 3 ----------------
+//Ejercicio 1: Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+const personAccount = {
+    firstName: "Marcos",
+    lastName: "Hernandez",
+    incomes: [
+        {
+            description: 'sueldo',
+            income: 15000
+        }, 
+        {
+            description: 'venta',
+            income: 10000
+        },
+        {
+            description: 'prestamo',
+            income: 1000
+        }
+    ],
+    expenses: [
+        {
+            description: 'despensa',
+            expense: 5000
+        },
+        {
+            description: 'transporte',
+            expense: 500
+        },
+        {
+            description: 'comida',
+            expense: 2000
+        }
+    ],
+    totalIncome: function(){
+        let arrIncomes = [];
+        const incomeDetail = this.incomes.map(income => {
+            arrIncomes.push(income.income);
+        })
+        return {"Total Incomes": arrIncomes.reduce((acc, curr) => acc + curr)};
+    },
+    totalExpense: function(){
+        let arrExpenses = [];
+        const expenseDetail = this.expenses.map(expense => {
+            arrExpenses.push(expense.expense);
+        })
+        return {"Total Expenses": arrExpenses.reduce((acc, curr) => acc + curr)};
+    },
+    accountInfo: function(){
+        return { total: this.totalIncome()["Total Incomes"] - this.totalExpense()["Total Expenses"]}
+    },
+    addIncome: function(income){
+        /* const newIncomes = [...this.incomes];
+        newIncomes.push(income); */
+        return this.incomes.push(income)
+    },
+    addExpense: function(expense){
+        return this.expenses.push(expense)
+    },
+    accountBalance: function(){
+        return { total: this.totalIncome()["Total Incomes"] - this.totalExpense()["Total Expenses"]}
+    }
+}
+console.log(personAccount.accountInfo());
+personAccount.addIncome({description:"otros", income: 5000})
+personAccount.addExpense({description:"medico", expense: 1000})
+console.log(personAccount.accountBalance());
+
+const personAccountJson = JSON.stringify(personAccount, undefined, 2);
+localStorage.setItem('person1', personAccountJson);
