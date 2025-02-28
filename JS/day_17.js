d957af6adb1cd4991827051a594c6e5f3312e864
Miguel Ruiz <later481@gmail.com>
2025-02-28 13:26:15 -0600

    Day 17 Level 2 completed

--- JS/day_17.js
+++ JS/day_17.js
@@ -11,3 +11,17 @@ const myPersonalDates = {
 }
 const userStringify = JSON.stringify(myPersonalDates, undefined, 4);
 localStorage.setItem('user', userStringify);
+
+
+//------------ LEVEL 2 ----------------
+//Ejercicio 1: Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
+const student = {
+    firstName: "Arturo",
+    lastName: "Perez",
+    age: 15,
+    skills: ["React", "Angular", "SQL"],
+    country: "USA",
+    enrolled: true
+}
+const studentStringify = JSON.stringify(student, undefined, 4);
+localStorage.setItem("student", studentStringify);
