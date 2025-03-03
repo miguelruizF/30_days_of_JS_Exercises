409b740355df3d1422bf7b518178a358ebbae8d0
Miguel Ruiz <later481@gmail.com>
2025-03-03 16:06:37 -0600

    Day 18 Level 3 Ejercicio 2 completed

--- JS/day_18.js
+++ JS/day_18.js
@@ -5,7 +5,28 @@ const countriesAPI = 'https://restcountries.com/v2/all';
 const catsAPI = 'https://api.thecatapi.com/v1/breeds';
 
 //Ejercicio 1:  Read the countries API using fetch and print the name of country, capital, languages, population and area.
-const readCountriesAPI = fetch(countriesAPI)
+const readCountries = async () => {
+    try {
+        const response = await fetch(countriesAPI)
+        const data = await response.json()
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Primera forma
+async function readCountriesAPI (){
+    const countriesShow = await readCountries();
+    countriesShow.forEach(element => {
+        const {name, capital, languages, population, area} = element;
+        console.log(`Country: ${name}, Capital: ${capital}, Languages: ${languages.map(language => language.name)}, Population: ${population}, Area: ${area}`);
+    });
+}
+// readCountriesAPI();
+
+//Segunda forma
+/* const readCountriesAPI = fetch(countriesAPI)
     .then(response => response.json())
     .then(data => {
         data.forEach(element => {
@@ -13,7 +34,7 @@ const readCountriesAPI = fetch(countriesAPI)
             console.log(`Country: ${name}, Capital: ${capital}, Languages: ${languages.map(language => language.name)}, Population: ${population}, Area: ${area}`);
         });
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error)); */
 
 
 //------------ LEVEL 2 ----------------
@@ -23,17 +44,18 @@ const readCatsAPI = async () => {
     try {
         const response = await fetch(catsAPI)
         const data = await response.json()
-        data.forEach( cat => {
+            data.forEach( cat => {
             const { name } = cat;
             catNames.push(name)
         } )
+        // return data;
         // console.log(data.name)        
     } catch (error) {
         console.log(error);
     }
     console.log(catNames);
 }
-readCatsAPI();
+// readCatsAPI();
 
 
 //------------ LEVEL 3 ----------------
@@ -56,4 +78,14 @@ const averageCats = async () => {
     }
     // console.log(catNames);
 }
-averageCats();
+// averageCats();
+
+//Ejercicio 2: Read the countries api and find out the 10 largest countries
+async function countriesLargest(){
+    const countriesFind = await readCountries();
+    console.log(countriesFind.map( country => {
+        const { name, population } = country;
+        return {name: name, population: population}
+    }).sort((a,b) => b.population - a.population).slice(0, 10))
+}
+countriesLargest();
