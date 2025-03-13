13cff31cd51364feb13e7e3789ee39e226f88e0c
Miguel Ruiz <later481@gmail.com>
2025-03-13 16:53:10 -0600

    Day 22 Level 1 completed

---
+++ JS/day_22/Level_1/app.js
@@ -0,0 +1,39 @@
+d = document;
+
+let ul_container = d.querySelector('ul');
+let li;
+
+d.addEventListener('DOMContentLoaded', () => {
+    createNumbers(1, 151)
+}); 
+
+function createNumbers(start, final){
+    if(start >= final){
+        console.log('start debe ser menor que final')
+        return;
+    } 
+    for (let index = start; index < final; index++) {
+        li = d.createElement('li');
+        li.textContent = index;
+        ul_container.appendChild(li);
+        if( index % 2 == 0){
+            li.style.backgroundColor = '#69fb6b'
+        }else if( index % 2 !== 0){
+            li.style.backgroundColor = '#fdf910'
+        }
+        if( isPrimeNumber(index) ){
+            li.style.backgroundColor = '#ff5757'
+        }
+    }
+}
+function isPrimeNumber(no) {
+    if (no < 2) {
+        return false;
+    }
+    for (let i = 2; i < no; i++) {
+        if (no % i == 0) {
+        return false;
+        }
+    }
+    return true;
+}
