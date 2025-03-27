7229a46ec875f14e5e14c31cea55c12043a2ae2d
miguelruizF <later481@gmail.com>
2025-03-26 20:53:05 -0600

    mostrarAlerta function created

--- JS/day_23/generating_numbers/app.js
+++ JS/day_23/generating_numbers/app.js
@@ -2,6 +2,7 @@ d = document;
 // let ul_container = d.querySelector('ul');
 let li;
 let wrapper = d.querySelector('.wrapper');
+let form_container = d.querySelector('.form_container');
 let formulario = d.querySelector("#formulario");
 let input_Value = d.querySelector("#input_numbers");
 
@@ -12,17 +13,23 @@ d.addEventListener('DOMContentLoaded', () => {
 
 function validacionFormulario(e){
     e.preventDefault();
-    if(Number(input_Value.value) <= 0 || input_Value.value.trim === "" || typeof(input_Value.value) == 'string'){
-        alert('ingresa un valor o caracter valido');
+    // Verificar si el valor ingresado es un número válido
+    if (isNaN(input_Value.value) || input_Value.value.trim() === "") {
+        mostrarAlerta('Por favor, ingresa un número válido.');
         return;
     }
 
-    let ul = d.querySelector('.ul_container');
-        if(ul){
-            wrapper.removeChild(ul);
-            createNumbers(0, obtainValues());
-        }
-        console.log(typeof(input_Value.value))
+    // Verificar si el número es mayor a 0
+    if (Number(input_Value.value) <= 0) {
+        mostrarAlerta('Por favor, ingresa un número mayor a 0.');
+        return;
+    }
+
+let ul = d.querySelector('.ul_container');
+if(ul){
+    wrapper.removeChild(ul);
+}
+createNumbers(0, obtainValues());
 }
 
 function createNumbers(start, final){
@@ -64,4 +71,18 @@ function obtainValues() {
     // e.preventDefault();
     let input = Number(d.querySelector("#input_numbers").value);
     return input;
+}
+
+function mostrarAlerta(mensaje){
+    const alerta = d.querySelector('.alerta');
+    if(!alerta){
+        const alerta = d.createElement('p');
+        alerta.textContent = mensaje;
+        alerta.classList.add('alerta');
+        // Insertar la alerta antes del input
+        form_container.insertBefore(alerta, formulario);
+        setTimeout(() => {
+            alerta.remove();
+        }, 2000);
+    }
 }
