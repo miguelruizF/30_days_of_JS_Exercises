ee4cb40507a01d9c8b9e2a25532d6128ccec3c7c
miguelruizF <later481@gmail.com>
2025-03-29 22:33:33 -0600

    function fetch-url created

---
+++ JS/day_24/app.js
@@ -0,0 +1,31 @@
+const url = 'http://localhost:3000/planetas';
+
+const obtainPlanets = async() => {
+    try {
+        const result = await fetch(url);
+        const planet = await result.json();
+        console.log(planet)
+    } catch (error) {
+        console.log(error);
+    }
+}
+obtainPlanets()
+
+/* (function(){
+    document.addEventListener('DOMContentLoaded', () => {
+        document.addEventListener('submit', showPlanet);
+    });
+
+    async function showPlanet(e){
+        e.preventDefault();
+        const planets = await obtainPlanets();
+
+        console.log(planets)
+        planets.forEach(planet => {
+            console.log(planet)
+        });
+    }
+
+
+})();
+ */
