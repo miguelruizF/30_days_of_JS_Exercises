072bc2af2eeaa4e41a8e1d172e3018201992fc8b
miguelruizF <later481@gmail.com>
2025-04-08 21:52:42 -0600

    functions to obtain completed planet information

--- JS/day_24/app.js
+++ JS/day_24/app.js
@@ -22,22 +22,21 @@ function validarFormulario(e){
     }
 
     if(isNaN(input_mass.value) || input_mass.value.trim() === ""){
-        // if(text_alert.classList.contains('hide')){
-        //     text_alert.classList.remove('hide');
-        // }
-        // text_alert.textContent = 'Ingresa un valor';
         text_content.textContent = 'Ingresa un valor';
+        text_content.classList.add('text_alert');
         info_content.appendChild(text_content);
         return;
     }
     if(Number(input_mass.value <= 0)){
         text_content.textContent = 'Ingresa un valor mayor a cero';
+        text_content.classList.add('text_alert');
         info_content.appendChild(text_content);
         return;
     }
 
     if(select_planet.selectedIndex === 0 || select_planet.value === ""){
         text_content.textContent = 'Selecciona un planeta';
+        text_content.classList.add('text_alert');
         info_content.appendChild(text_content);
         return;
     }
@@ -54,10 +53,30 @@ async function obtenerPlaneta(id_planet) {
     const contenedor_info = document.createElement('div');
     contenedor_info.classList.add('contenedor_planeta');
     
+    //Imagen del planeta
     const img_planet = document.createElement('img');
     img_planet.src = img;
-    img_planet.id = 'img_planet'
+    img_planet.id = 'img_planet';
 
+    //Informacion del peso
+    let peso = Number(input_mass.value) * gravedad;
+
+    const div_info = document.createElement('div');
+    div_info.classList.add('div_info');
+
+    const texto_planeta = document.createElement('p');
+    texto_planeta.classList.add('texto_planeta');
+    texto_planeta.innerHTML = `The weight of the object on <span class="nombre_planeta">${nombre}</span>`
+
+    const masa_planeta = document.createElement('p');
+    masa_planeta.classList.add('masa_planeta');
+    masa_planeta.textContent = `${Number(peso.toFixed(2))} N`;
+
+    div_info.appendChild(texto_planeta);
+    div_info.appendChild(masa_planeta);
+
+    //Agregar elementos al contenedor principal 
     contenedor_info.appendChild(img_planet);
+    contenedor_info.appendChild(div_info);
     info_content.appendChild(contenedor_info);
 }
