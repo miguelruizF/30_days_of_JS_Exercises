8fbf23af9fbadc3ace650bd1caef3879d5a467ad
miguelruizF <later481@gmail.com>
2025-04-15 23:02:09 -0600

    Creation of the chart

--- JS/day_25/app.js
+++ JS/day_25/app.js
@@ -1,3 +1,32 @@
-import { countries_data } from "countries_data.js";
+import { countries_data } from "./data/countries_data.js";
 
+const chart = document.getElementById('myChart');
 
+// document.addEventListener('DOMContentLoaded', showCanvas);
+
+const countries = countries_data.map(country => {
+    const { name, languages, population } = country;
+    return {name: name, population: population};
+}).sort((a,b) => b.population - a.population).slice(0,10);
+
+new Chart(chart, {
+    type: 'bar',
+    data: {
+        labels: countries.map(row => row.name),
+        datasets: [{
+        label: '# of Votes',
+        data: countries.map(row => row.population),
+        borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis: 'y',
+        scales: {
+            y: {
+                ticks: {
+                    crossAlign: 'far'
+                }
+            }
+        }
+    }
+});
