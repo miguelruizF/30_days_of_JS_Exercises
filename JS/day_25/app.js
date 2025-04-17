1601dbd8cbed9356011da57aef439707fb65ca5c
miguelruizF <later481@gmail.com>
2025-04-16 22:14:39 -0600

    Creating a function to generate a population chart

--- JS/day_25/app.js
+++ JS/day_25/app.js
@@ -1,32 +1,53 @@
 import { countries_data } from "./data/countries_data.js";
 
-const chart = document.getElementById('myChart');
+const btnPopulation = document.getElementById('population');
+const btnLanguages = document.getElementById('languages');
+const container = document.getElementById('container');
 
-// document.addEventListener('DOMContentLoaded', showCanvas);
+document.addEventListener('DOMContentLoaded', () => {
+    btnPopulation.addEventListener('click', createPopulation);
+});
 
-const countries = countries_data.map(country => {
-    const { name, languages, population } = country;
-    return {name: name, population: population};
-}).sort((a,b) => b.population - a.population).slice(0,10);
+const createPopulation = () => {
+    //Si existe un elemento hijo previo, lo elimina
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 
-new Chart(chart, {
-    type: 'bar',
-    data: {
-        labels: countries.map(row => row.name),
-        datasets: [{
-        label: '# of Votes',
-        data: countries.map(row => row.population),
-        borderWidth: 1
-        }]
-    },
-    options: {
-        indexAxis: 'y',
-        scales: {
-            y: {
-                ticks: {
-                    crossAlign: 'far'
+    //Creacion del canvas
+    const canvas = document.createElement('canvas');
+    canvas.id = 'myChart';
+    container.appendChild(canvas);
+
+    //obtencion de datos
+    const countries = countries_data.map(country => {
+        const { name, languages, population } = country;
+        return {name: name, population: population};
+    }).sort((a,b) => b.population - a.population).slice(0,10);
+    
+    //Creacion de la grafica con chart js
+    const chart = document.getElementById('myChart');
+    new Chart(chart, {
+        type: 'bar',
+        data: {
+            labels: countries.map(row => row.name),
+            datasets: [{
+                label: '# of Population',
+                data: countries.map(row => row.population),
+                borderWidth: 1,
+                backgroundColor: '#feaa3f'
+            }]
+        },
+        options: {
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    ticks: {
+                        crossAlign: 'far'
+                    }
                 }
             }
         }
-    }
-});
+    });
+    
+}
