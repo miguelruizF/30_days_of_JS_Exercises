d16feff9109142a93b7bbab6795e3339eb5946ef
miguelruizF <later481@gmail.com>
2025-04-18 21:03:20 -0600

    creation of the creationLanguages function

--- JS/day_25/app.js
+++ JS/day_25/app.js
@@ -6,6 +6,7 @@ const container = document.getElementById('container');
 
 document.addEventListener('DOMContentLoaded', () => {
     btnPopulation.addEventListener('click', createPopulation);
+    btnLanguages.addEventListener('click', createLanguages);
 });
 
 const createPopulation = () => {
@@ -24,7 +25,7 @@ const createPopulation = () => {
         const { name, languages, population } = country;
         return {name: name, population: population};
     }).sort((a,b) => b.population - a.population).slice(0,10);
-    
+
     //Creacion de la grafica con chart js
     const chart = document.getElementById('myChart');
     new Chart(chart, {
@@ -50,4 +51,57 @@ const createPopulation = () => {
         }
     });
     
+}
+
+const createLanguages = () => {
+    //Si existe un elemento hijo previo, lo elimina
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    //Creacion del canvas
+    const canvas = document.createElement('canvas');
+    canvas.id = 'myChart';
+    container.appendChild(canvas);
+
+    //obtencion de datos
+    let langs = [];
+    countries_data.map(country => {
+        const { languages } = country;
+        languages.forEach(lang => langs.push(lang))
+    });
+
+    const langsCount = langs.reduce((acc, curr) => {
+        acc[curr]= (acc[curr] || 0) + 1;
+        return acc;
+    },{});
+
+    const mode = Object.entries(langsCount)
+        .sort((a,b) => b[1] - a[1])
+        .slice(0,10);
+
+    //Creacion de la grafica con chart js
+    const chart = document.getElementById('myChart');
+    new Chart(chart, {
+        type: 'bar',
+        data: {
+            labels: mode.map(row => row[0]),
+            datasets: [{
+                label: '# of Languages',
+                data: mode.map(row => row[1]),
+                borderWidth: 1,
+                backgroundColor: '#feaa3f'
+            }]
+        },
+        options: {
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    ticks: {
+                        crossAlign: 'far'
+                    }
+                }
+            }
+        }
+    });
 }
