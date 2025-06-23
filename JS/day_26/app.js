aa779351276f052539b97f60f1032f5b8c9c754e
miguelruizF <later481@gmail.com>
2025-06-22 18:17:14 -0600

    Day 26: List of countries finished

--- JS/day_26/app.js
+++ JS/day_26/app.js
@@ -1,1 +1,16 @@
 import { countries } from "./data/countries.js";
+let cards_container = document.querySelector('#cards');
+
+document.addEventListener('DOMContentLoaded', () => {
+    showCountries();
+});
+
+function showCountries(){
+    const countries_map = countries.map(country => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.textContent = country;
+
+        cards_container.appendChild(card);
+    });
+}
