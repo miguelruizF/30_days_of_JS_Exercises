4f5a092a985cbc68164da6ba2c2d1c1489adf8d4
miguelruizF <later481@gmail.com>
2025-06-22 21:44:19 -0600

    Day 26: searching country with a word completed

--- JS/day_26/app.js
+++ JS/day_26/app.js
@@ -1,11 +1,17 @@
 import { countries } from "./data/countries.js";
 let cards_container = document.querySelector('#cards');
 let btn_order = document.querySelector('#order');
+let formulario = document.querySelector('#formulario');
+let btn_startWord = document.querySelector('#start');
+
 let ascending = true;
+let isSelected = false;
+let searchString = "";
 
 document.addEventListener('DOMContentLoaded', () => {
     showCountries();
     orderCountries();
+    btn_startWord.addEventListener('click', searchOfWord);
 });
 
 function showCountries(){
@@ -24,5 +30,39 @@ function orderCountries(){
         countries.sort((a, b) => ascending ? a.localeCompare(b) : b.localeCompare(a)); //El localCompare se usa para comparar los string
         ascending = !ascending; //Alterna el orden en el siguiente clic
         showCountries();
+    });
+}
+
+function searchOfWord(e){
+    e.preventDefault();
+    isSelected = !isSelected;
+    if(isSelected){
+        btn_startWord.style.backgroundColor = '#5d09e4';
+        searchString = "";
+        document.addEventListener('keydown', searchingCountry);
+    }else{
+        btn_startWord.style.backgroundColor = '';
+        document.removeEventListener('keydown', searchingCountry);
+    }
+}
+
+function searchingCountry(e){
+    if(e.key.length === 1){
+        searchString += e.key;
+    }else if(e.key === 'Backspace'){
+        searchString = searchString.slice(0, -1);
+    }else{
+        return;
+    }
+    
+    cards_container.innerHTML = '';
+    const filtered = countries.filter(country => country.toLowerCase().startsWith(searchString.toLowerCase()));
+
+    cards_container.innerHTML = '';
+    filtered.forEach(country => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.textContent = country;
+        cards_container.appendChild(card);
     });
 }
