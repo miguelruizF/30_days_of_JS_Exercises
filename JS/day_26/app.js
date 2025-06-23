e1a11a71333b8878b59d68041a32207e82410a66
miguelruizF <later481@gmail.com>
2025-06-22 19:09:34 -0600

    Day 26: ascending function completed

--- JS/day_26/app.js
+++ JS/day_26/app.js
@@ -1,16 +1,28 @@
 import { countries } from "./data/countries.js";
 let cards_container = document.querySelector('#cards');
+let btn_order = document.querySelector('#order');
+let ascending = true;
 
 document.addEventListener('DOMContentLoaded', () => {
     showCountries();
+    orderCountries();
 });
 
 function showCountries(){
-    const countries_map = countries.map(country => {
+    cards_container.innerHTML = "";
+    countries.map(country => {
         const card = document.createElement('div');
         card.classList.add('card');
         card.textContent = country;
 
         cards_container.appendChild(card);
+    });
+}
+
+function orderCountries(){
+    btn_order.addEventListener('click', () => {
+        countries.sort((a, b) => ascending ? a.localeCompare(b) : b.localeCompare(a)); //El localCompare se usa para comparar los string
+        ascending = !ascending; //Alterna el orden en el siguiente clic
+        showCountries();
     });
 }
