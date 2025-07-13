f2262544f65c3131936d4b30563eb17a3bcd6f5d
miguelruizF <later481@gmail.com>
2025-07-12 19:43:53 -0600

    Day 27: biography completed

--- JS/day_27/app.js
+++ JS/day_27/app.js
@@ -1,8 +1,17 @@
 let typing_text = document.querySelector('.text_profession');
+let typing_techs = document.querySelector('.techs_content');
 
 const typing = new Typed(typing_text, {
     strings: [' ','💻 Developer', '👷 Engineer Support'],
     typeSpeed: 50,
     backSpeed: 40,
     loop: true,
+});
+
+const typing_tech = new Typed(typing_techs, {
+    strings: ['Javascript', 'React', 'Mongo', 'Angular'],
+    typeSpeed: 50,
+    backSpeed: 40,
+    loop: true,
+    showCursor: false
 })
